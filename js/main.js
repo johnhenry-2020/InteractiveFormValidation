0fed2c28e1f75fdd343bea0ac941a6c8e764d7e7
JJ <jonjolivette777@aol.com>
2019-07-19 00:44:21 -0500

    set focus on form's first text field

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
+//jquery sanity check to insure code is linked and functioning properly
 $(document).ready(function() {
 	alert('sanity check for Jquery');
 });
+
+// Background images set to rotate via cycle.js library
 $('#slideshow').cycle({
 	speed: 1800,
 	timeout: 3500
 });
+
+// When the page first loads, this sets the first text field in focus by default.
+$('#name').focus();
